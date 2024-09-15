b7f21d89ea53cd79382531933eb8460aed5fa4ee
devmahin <mahinhowlader123@gmail.com>
2024-09-15 23:59:15 +0530

    add product

--- src/DashboardComponent/Allitems/Allitems.jsx
+++ src/DashboardComponent/Allitems/Allitems.jsx
@@ -1,7 +1,77 @@
 function Allitems() {
     return (
         <div>
-            All Items
+            <div>
+            <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+    <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Product settings</h2>
+
+    <form>
+        <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="productName">Product Name</label>
+                <input
+                    id="productName"
+                    type="text"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+            </div>
+
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="imageUrl">Image URL</label>
+                <input
+                    id="imageUrl"
+                    type="text"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+            </div>
+
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="newPrice">New Price</label>
+                <input
+                    id="newPrice"
+                    type="number"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+            </div>
+
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="oldPrice">Old Price</label>
+                <input
+                    id="oldPrice"
+                    type="number"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+            </div>
+
+            <div className="sm:col-span-2">
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="description">Description</label>
+                <textarea
+                    id="description"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+            </div>
+
+            <div className="sm:col-span-2">
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="category">Category</label>
+                <select
+                    id="category"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                >
+                    <option value="women">Women</option>
+                    <option value="men">Men</option>
+                    <option value="kid">Kid</option>
+                </select>
+            </div>
+        </div>
+
+        <div className="flex justify-end mt-6">
+            <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
+        </div>
+    </form>
+</section>
+
+
+            </div>
         </div>
     );
 }
